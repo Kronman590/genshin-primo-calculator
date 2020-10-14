0663e132e2f45fb0e0acef716a3af3dc899bc553
jw97 <jake.wong@alumni.ubc.ca>
2020-10-14 01:06:42 -0700

    Initial commit

---
+++ characters.js
@@ -0,0 +1,55 @@
+var Amber = {
+    "1Hit": {dmg:36.1,elemental:false},
+    "2Hit": {dmg:36.1,elemental:false},
+    "3Hit": {dmg:46.4,elemental:false},
+    "4Hit": {dmg:47.3,elemental:false},
+    "5Hit": {dmg:59.3,elemental:false},
+    "Aimed": {dmg:43.9,elemental:false},
+    "Charged": {dmg:124,elemental:true},
+    "LowPlunge": {dmg:114,elemental:false},
+    "HighPlunge": {dmg:142,elemental:false},
+    "Skill": {dmg:123,elemental:true},
+    "Burst": {dmg:505,elemental:true}
+}
+
+var Barbara = {
+    "1Hit": {dmg:37.8,elemental:true},
+    "2Hit": {dmg:35.5,elemental:true},
+    "3Hit": {dmg:41,elemental:true},
+    "4Hit": {dmg:55.2,elemental:true},
+    "Charged": {dmg:166,elemental:true},
+    "LowPlunge": {dmg:114,elemental:true},
+    "HighPlunge": {dmg:142,elemental:true},
+    "Skill": {dmg:58.4,elemental:true},
+}
+
+var Beidou = {
+    "1Hit": {dmg:71.1,elemental:false},
+    "2Hit": {dmg:70.9,elemental:false},
+    "3Hit": {dmg:88.3,elemental:false},
+    "4Hit": {dmg:86.5,elemental:false},
+    "5Hit": {dmg:112,elemental:false},
+    "Charged": {dmg:102,elemental:false},
+    "LowPlunge": {dmg:149,elemental:false},
+    "HighPlunge": {dmg:186,elemental:false},
+    "Skill": {dmg:122,elemental:true},
+    "Burst": {dmg:122,elemental:false},
+    "BurstSec": {dmg:96,elemental:true}
+}
+
+var temp = {
+    "1Hit": {dmg:000000,elemental:false},
+    "2Hit": {dmg:000000,elemental:false},
+    "3Hit": {dmg:000000,elemental:false},
+    "4Hit": {dmg:000000,elemental:false},
+    "5Hit": {dmg:000000,elemental:false},
+    "Aimed": {dmg:43.9,elemental:false},
+    "Charged": {dmg:000000,elemental:true},
+    "LowPlunge": {dmg:000000,elemental:false},
+    "HighPlunge": {dmg:000000,elemental:false},
+    "Skill": {dmg:000000,elemental:true},
+    "SkillHold": {dmg:000000,elemental:true},
+    "SkillMulti": {dmg:000000,elemental:true},
+    "Burst": {dmg:000000,elemental:false},
+    "BurstSec": {dmg:000000,elemental:true}
+}
