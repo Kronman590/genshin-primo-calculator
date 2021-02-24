762411fd1bbf5a38e76f608dff03dc88c7482bf1
jw97 <jake.wong@alumni.ubc.ca>
2021-02-24 12:59:37 -0800

    Events tab

--- events.js
+++ events.js
@@ -12,3 +12,32 @@ var events = [["Wish Upon a Lantern","2021-02-26",100],
 ["Vishaps and Where to Find Them","2021-03-09",20],
 ["Vishaps and Where to Find Them","2021-03-10",20],
 ["Venti Trial","2021-03-17",20]];
+
+function createTable() {
+    var table = document.getElementById("events");
+    var eventNames = [];
+    for (var e in events) {
+        if(eventNames.indexOf(events[e][0]) < 0)
+            eventNames.push(events[e][0]);
+    }
+    for (var n in eventNames) {
+        var row = table.insertRow(-1);
+        ename = eventNames[n];
+        row.insertCell(0).innerHTML = ename;
+        var gems = 0;
+        var start = "";
+        var end = "";
+        for (e in events) {
+            if(events[e][0] == ename) {
+                gems += events[e][2];
+                end = events[e][1];
+                if(start == "") {
+                    start = events[e][1];
+                }
+            }
+        }
+        row.insertCell(1).innerHTML = start;
+        row.insertCell(2).innerHTML = end;
+        row.insertCell(3).innerHTML = gems;
+    }
+}
