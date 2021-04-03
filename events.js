12139cb90af61e536b85df82ae9f92e0d0ecf51f
jw97 <jake.wong@alumni.ubc.ca>
2021-04-03 12:38:05 -0700

    Event Guesses for April

--- events.js
+++ events.js
@@ -6,8 +6,21 @@ var events = [["Contending Tides","2021-04-02",60],
 ["Mihoyo Web Login","2021-04-06",20],
 ["Contending Tides","2021-04-07",60],
 ["Contending Tides","2021-04-08",60],
-["Mihoyo Web Login","2021-03-13",20],
-["Mihoyo Web Login","2021-03-20",20]];
+["Wishful Drops","2021-04-09",60],
+["Wishful Drops","2021-04-11",60],
+["Wishful Drops","2021-04-13",60],
+["Archon Quest Act 1 Ch 4","2021-04-12",60],
+["Mihoyo Web Login","2021-04-13",20],
+["1.5 Livestream Codes","2021-04-16",300],
+["Marvelous Merchandise","2021-04-16",40],
+["Marvelous Merchandise","2021-04-17",40],
+["Marvelous Merchandise","2021-04-18",40],
+["Marvelous Merchandise","2021-04-19",40],
+["Marvelous Merchandise","2021-04-20",140],
+["Mihoyo Web Login","2021-04-20",20],
+["Marvelous Merchandise","2021-04-21",40],
+["Marvelous Merchandise","2021-04-22",40],
+["1.5 Patch Compensation","2021-04-28",600]];
 
 function createTable() {
     var table = document.getElementById("events");
